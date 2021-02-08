77149687b18825b17e9c02442f238725f332073c
MarcelloVendruscolo <marcello.pietro@hotmail.com>
2021-02-08 16:52:16 +0200

    Separated the Mongo Shell section to a JavaScript file

--- Assignment_2/mongodb.py
+++ Assignment_2/mongodb.py
@@ -44,29 +44,3 @@ for entry in entries:
             f.close() # close file handle
 
 client.close # close db connection
-
-%%bash
-mongo tweets_db
-var mapper = function() {
-    var pronouns = ["HAN", "HON", "HEN", 'DEN', 'DET', 'DENNA', 'DENNE'];
-    db.tweets.createIndex({ text: "text" });
-    if (this.text) {
-        for (var counter = 0; counter < pronouns.length; counter++) {
-            var match = 0;
-            match = this.find( { $text: { $search: "\"" +pronouns[counter]+ "\"" } }).count();
-            if (match !== 0) {
-                emit(pronouns[counter], 1);
-            } else {
-                emit(pronouns[counter], 0);
-            }
-        }
-        emit('tweets_count', 1);
-    }
-}
-
-var reducer = function (key,values) {
-    return Array.sum(values);
-}
-
-db.tweets.mapReduce(mapper, reducer, { query: { }, out: "pronouns_count" })
-db.pronouns_count.find()
