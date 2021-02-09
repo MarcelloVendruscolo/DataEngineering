55de5d732abc11cbacec98325c8844c858d6bc78
MarcelloVendruscolo <marcello.pietro@hotmail.com>
2021-02-09 09:18:44 +0200

    Modified from Map to List because of support from Mongo Shell

--- Assignment_2/mongobash.js
+++ Assignment_2/mongobash.js
@@ -2,25 +2,31 @@ conn = new Mongo();
 db = conn.getDB('tweets_db');
 
 var mapper = function() {
-    var map_pronouns = new Map()
-    var list_pronouns = ['han', 'hon', 'hen', 'den', 'det', 'denna', 'denne'];
-    list_pronouns.forEach(function(pronoun) {
-        map_pronouns.put(pronoun, 0);
-    })
-    var tweet_text = this.text
-    for (let pronoun of map_pronouns.keys()) {
+    var tweet_text = this.text;
+
+    var list_pronouns = [{pronoun:'han', count:0},
+    {pronoun:'hon', count:0},
+    {pronoun:'hen', count:0},
+    {pronoun:'den', count:0},
+    {pronoun:'det', count:0},
+    {pronoun:'denna', count:0},
+    {pronoun:'denne', count:0}];
+
+    list_pronouns.forEach(function(object) {
         var match = 0;
-        let re = new RegExp('\\b' +pronoun+ '\\b', 'i');
-        match = tweet_text.match(re)
+        let re = new RegExp('\\b' +object.pronoun+ '\\b', 'i');
+        match = tweet_text.match(re);
         if (match !== null) {
-            map_pronouns.put(pronoun, 1)
+            object.count = 1;
         }
-        emit(pronoun, map_pronouns.get(pronoun));
-    }
+        emit(object.pronoun, object.count);
+    })
     emit('tweets_count', 1);
 }
+
 var reducer = function (key,values) {
     return Array.sum(values);
 }
+
 db.tweets.mapReduce(mapper, reducer, { query: { }, out: "pronouns_count" })
 db.pronouns_count.find().pretty()
