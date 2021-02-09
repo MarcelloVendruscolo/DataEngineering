c95606546254e5a968c306b4f73699115a478ebb
MarcelloVendruscolo <marcello.pietro@hotmail.com>
2021-02-09 09:41:59 +0200

    Export query result in JSON format

--- Assignment_2/mongobash.js
+++ Assignment_2/mongobash.js
@@ -29,4 +29,4 @@ var reducer = function (key,values) {
 }
 
 db.tweets.mapReduce(mapper, reducer, { query: { }, out: "pronouns_count" })
-db.pronouns_count.find().pretty()
+printjson(db.pronouns_count.find().pretty())
