6eb2f13d7d7496fa3b37439091867fdbca1bf6b6
MarcelloVendruscolo <marcello.pietro@hotmail.com>
2021-02-11 20:25:31 +0200

    Broken Regex

--- Assignment_2/mongobash.js
+++ Assignment_2/mongobash.js
@@ -6,7 +6,7 @@ var mapper = function() {
     var list_pronouns = [{pronoun:'han', count:0}, {pronoun:'hon', count:0}, {pronoun:'hen', count:0}, {pronoun:'den', count:0}, {pronoun:'det', count:0}, {pronoun:'denna', count:0}, {pronoun:'denne', count:0}];
 
     list_pronouns.forEach(function(object) {
-        var re = new RegExp(String.raw`\b${object.pronoun}\b`, 'i');
+        var re = new RegExp(object.pronoun, 'i');
         var match = tweet_text.match(re);
         if (match !== null) {
             object.count = 1;
@@ -20,5 +20,5 @@ var reducer = function (key,values) {
     return Array.sum(values);
 }
 
-db.tweets.mapReduce(mapper, reducer, { query: { }, out: "pronouns_count" })
-printjson(db.pronouns_count.find().toArray())
+db.tweets.mapReduce(mapper, reducer, { query: { }, out: "pronouns_count" });
+printjson(db.pronouns_count.find().toArray());
