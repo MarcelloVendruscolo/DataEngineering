d8aa62e78417b4f7b5114236b1522480e3c505b7
MarcelloVendruscolo <marcello.pietro@hotmail.com>
2021-02-11 21:05:16 +0200

    Formatting file

--- Assignment_2/mongobash.js
+++ Assignment_2/mongobash.js
@@ -3,7 +3,13 @@ db = conn.getDB('tweets_db');
 
 var mapper = function() {
     var tweet_text = this.text;
-    var list_pronouns = [{pronoun:'han', count:0}, {pronoun:'hon', count:0}, {pronoun:'hen', count:0}, {pronoun:'den', count:0}, {pronoun:'det', count:0}, {pronoun:'denna', count:0}, {pronoun:'denne', count:0}];
+    var list_pronouns = [{pronoun:'han', count:0},
+    {pronoun:'hon', count:0},
+    {pronoun:'hen', count:0},
+    {pronoun:'den', count:0},
+    {pronoun:'det', count:0},
+    {pronoun:'denna', count:0},
+    {pronoun:'denne', count:0}];
 
     list_pronouns.forEach(function(object) {
         var re = new RegExp(String.raw`\b${object.pronoun}\b`, 'i');
